92ed42f7a51f5bb20b3c312ad95a8fe6e301a81a
Gary Russell <grussell@pivotal.io>
2017-05-14 17:43:17 -0400

    GH-313: Configure group.id on @KafkaListener
    Resolves: https://github.com/spring-projects/spring-kafka/issues/313
    
    Allow configuration of the `group.id` property on the annotation.

--- spring-kafka/src/main/java/org/springframework/kafka/annotation/KafkaListenerAnnotationBeanPostProcessor.java
+++ spring-kafka/src/main/java/org/springframework/kafka/annotation/KafkaListenerAnnotationBeanPostProcessor.java
@@ -383,6 +383,7 @@ public class KafkaListenerAnnotationBeanPostProcessor<K, V>
 		endpoint.setBean(bean);
 		endpoint.setMessageHandlerMethodFactory(this.messageHandlerMethodFactory);
 		endpoint.setId(getEndpointId(kafkaListener));
+		endpoint.setGroupId(getEndpointGroupId(kafkaListener, endpoint.getId()));
 		endpoint.setTopicPartitions(resolveTopicPartitions(kafkaListener));
 		endpoint.setTopics(resolveTopics(kafkaListener));
 		endpoint.setTopicPattern(resolvePattern(kafkaListener));
@@ -418,6 +419,17 @@ public class KafkaListenerAnnotationBeanPostProcessor<K, V>
 		else {
 			return "org.springframework.kafka.KafkaListenerEndpointContainer#" + this.counter.getAndIncrement();
 		}
+	}
+
+	private String getEndpointGroupId(KafkaListener kafkaListener, String id) {
+		String groupId = null;
+		if (StringUtils.hasText(kafkaListener.groupId())) {
+			groupId = resolveExpressionAsString(kafkaListener.groupId(), "groupId");
+		}
+		if (groupId == null && kafkaListener.idIsGroup()) {
+			groupId = id;
+		}
+		return groupId;
 	}
 
 	private TopicPartitionInitialOffset[] resolveTopicPartitions(KafkaListener kafkaListener) {
