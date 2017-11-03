2b8a8ed1b42c14132cdaa8497adb5689b6198552
Gary Russell <grussell@pivotal.io>
2017-11-03 13:15:45 -0400

    GH-470: Add SeekToCurrentErrorHandler
    Fixes https://github.com/spring-projects/spring-kafka/issues/470
    
    2.0 introduced the `ConsumerAwareErrorHandler`, which allows the user to
    `seek()` the consumer after an exception.
    
    However, any pending records from the previous poll are still sent to
    the listener.
    
    Add the `RemainingRecordsErrorHandler` interface and `SeekToCurrentErrorHandler`
    implementation which resets the offsets of all unprocessed topic/partitions so
    that the next poll will return the failed record and all of the previously
    unprocessed records.
    
    __cherry-pick to 2.0.x__

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
@@ -419,6 +419,9 @@ public class KafkaMessageListenerContainer<K, V> extends AbstractMessageListener
 			}
 			this.monitorTask = this.taskScheduler.scheduleAtFixedRate(() -> checkConsumer(),
 					this.containerProperties.getMonitorInterval() * 1000);
+			Assert.state(this.transactionTemplate == null
+					|| !(this.errorHandler instanceof RemainingRecordsErrorHandler),
+						"You cannot use a 'RemainingRecordsErrorHandler' with transactions");
 		}
 
 		protected void checkConsumer() {
@@ -875,7 +878,7 @@ public class KafkaMessageListenerContainer<K, V> extends AbstractMessageListener
 								producer = ((KafkaResourceHolder) TransactionSynchronizationManager
 										.getResource(ListenerConsumer.this.kafkaTxManager.getProducerFactory())).getProducer();
 							}
-							RuntimeException aborted = doInvokeRecordListener(record, producer);
+							RuntimeException aborted = doInvokeRecordListener(record, producer, null);
 							if (aborted != null) {
 								throw aborted;
 							}
@@ -898,7 +901,7 @@ public class KafkaMessageListenerContainer<K, V> extends AbstractMessageListener
 				if (this.logger.isTraceEnabled()) {
 					this.logger.trace("Processing " + record);
 				}
-				doInvokeRecordListener(record, null);
+				doInvokeRecordListener(record, null, iterator);
 			}
 		}
 
@@ -907,11 +910,14 @@ public class KafkaMessageListenerContainer<K, V> extends AbstractMessageListener
 		 * @param record the record.
 		 * @param producer the producer - only if we're running in a transaction, null
 		 * otherwise.
+		 * @param iterator the {@link ConsumerRecords} iterator - used only if a
+		 * {@link RemainingRecordsErrorHandler} is being used.
 		 * @return an exception.
 		 * @throws Error an error.
 		 */
 		private RuntimeException doInvokeRecordListener(final ConsumerRecord<K, V> record,
-				@SuppressWarnings("rawtypes") Producer producer) throws Error {
+				@SuppressWarnings("rawtypes") Producer producer,
+				Iterator<ConsumerRecord<K, V>> iterator) throws Error {
 			try {
 				switch (this.listenerType) {
 					case ACKNOWLEDGING_CONSUMER_AWARE:
@@ -960,13 +966,24 @@ public class KafkaMessageListenerContainer<K, V> extends AbstractMessageListener
 					throw e;
 				}
 				try {
-					this.errorHandler.handle(e, record, this.consumer);
-					if (producer != null) {
-						try {
-							sendOffsetsToTransaction(producer);
+					if (this.errorHandler instanceof RemainingRecordsErrorHandler) {
+						processCommits();
+						List<ConsumerRecord<?, ?>> records = new ArrayList<>();
+						records.add(record);
+						while (iterator.hasNext()) {
+							records.add(iterator.next());
 						}
-						catch (Exception e1) {
-							this.logger.error("Send offsets to transaction failed", e1);
+						((RemainingRecordsErrorHandler) this.errorHandler).handle(e, records, this.consumer);
+					}
+					else {
+						this.errorHandler.handle(e, record, this.consumer);
+						if (producer != null) {
+							try {
+								sendOffsetsToTransaction(producer);
+							}
+							catch (Exception e1) {
+								this.logger.error("Send offsets to transaction failed", e1);
+							}
 						}
 					}
 				}
