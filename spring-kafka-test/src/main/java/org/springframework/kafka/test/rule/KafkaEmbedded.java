0a3ea64fd36cbaf8b84d51b220f595cc9e58c7db
Elliot Kennedy <me@elken.io>
2018-01-23 00:57:01 +0000

    GH-539 - Wait for partitions to be assigned when consuming from embedded topics
    - Add a test to consume from embedded topics.
    - Updated authors and copywrite.

--- spring-kafka-test/src/main/java/org/springframework/kafka/test/rule/KafkaEmbedded.java
+++ spring-kafka-test/src/main/java/org/springframework/kafka/test/rule/KafkaEmbedded.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2017 the original author or authors.
+ * Copyright 2015-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -72,6 +72,7 @@ import kafka.zk.EmbeddedZookeeper;
  * @author Artem Bilan
  * @author Gary Russell
  * @author Kamill Sokol
+ * @author Elliot Kennedy
  */
 public class KafkaEmbedded extends ExternalResource implements KafkaRule, InitializingBean, DisposableBean {
 
