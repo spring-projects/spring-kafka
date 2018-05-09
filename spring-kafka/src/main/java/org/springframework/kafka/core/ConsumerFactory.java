e3ec840a4637db10f897ee9b596ee6c8342e08e9
Artem Bilan <abilan@pivotal.io>
2018-05-09 15:17:22 -0400

    Resolve a couple TODOs

--- spring-kafka/src/main/java/org/springframework/kafka/core/ConsumerFactory.java
+++ spring-kafka/src/main/java/org/springframework/kafka/core/ConsumerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,16 +63,13 @@ public interface ConsumerFactory<K, V> {
 	 * {@code client.id} property, if present.
 	 * If a factory does not implement this method, {@link #createConsumer(String, String)}
 	 * is invoked, ignoring the prefix.
-	 * TODO: remove default in 2.2
 	 * @param groupId the group id.
 	 * @param clientIdPrefix the prefix.
 	 * @param clientIdSuffix the suffix.
 	 * @return the consumer.
 	 * @since 2.1.1
 	 */
-	default Consumer<K, V> createConsumer(String groupId, String clientIdPrefix, String clientIdSuffix) {
-		return createConsumer(groupId, clientIdSuffix);
-	}
+	Consumer<K, V> createConsumer(String groupId, String clientIdPrefix, String clientIdSuffix);
 
 	/**
 	 * Return true if consumers created by this factory use auto commit.
