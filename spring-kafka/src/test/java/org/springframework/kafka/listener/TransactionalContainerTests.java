4e61dc6f85e3797691c6ed52de5fc19d405820e3
Gary Russell <grussell@pivotal.io>
2018-12-10 10:30:45 -0500

    GH-900: Use correct txId for initial commit
    Fixes https://github.com/spring-projects/spring-kafka/issues/900
    
    Use the proper zombie-fencing `transational.id` when committing initial
    offsets after assignment.
    
    **cherry-pick to all, 1.3.x will probable need a backport**

--- spring-kafka/src/test/java/org/springframework/kafka/listener/TransactionalContainerTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/listener/TransactionalContainerTests.java
@@ -30,6 +30,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -153,7 +154,7 @@ public class TransactionalContainerTests {
 		ConsumerFactory cf = mock(ConsumerFactory.class);
 		willReturn(consumer).given(cf).createConsumer("group", "", null);
 		Producer producer = mock(Producer.class);
-		final CountDownLatch closeLatch = new CountDownLatch(1);
+		final CountDownLatch closeLatch = new CountDownLatch(2);
 		willAnswer(i -> {
 			closeLatch.countDown();
 			return null;
@@ -185,6 +186,11 @@ public class TransactionalContainerTests {
 		assertThat(closeLatch.await(10, TimeUnit.SECONDS)).isTrue();
 		InOrder inOrder = inOrder(producer);
 		inOrder.verify(producer).beginTransaction();
+		inOrder.verify(producer).sendOffsetsToTransaction(Collections.singletonMap(topicPartition,
+				new OffsetAndMetadata(0)), "group");
+		inOrder.verify(producer).commitTransaction();
+		inOrder.verify(producer).close();
+		inOrder.verify(producer).beginTransaction();
 		ArgumentCaptor<ProducerRecord> captor = ArgumentCaptor.forClass(ProducerRecord.class);
 		inOrder.verify(producer).send(captor.capture(), any(Callback.class));
 		assertThat(captor.getValue()).isEqualTo(new ProducerRecord("bar", "baz"));
@@ -193,7 +199,8 @@ public class TransactionalContainerTests {
 		inOrder.verify(producer).commitTransaction();
 		inOrder.verify(producer).close();
 		container.stop();
-		verify(pf, times(1)).createProducer();
+		verify(pf, times(2)).createProducer();
+		verifyNoMoreInteractions(producer);
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -433,7 +440,7 @@ public class TransactionalContainerTests {
 			}
 		});
 
-		@SuppressWarnings({ "rawtypes", "unchecked" })
+		@SuppressWarnings({ "rawtypes" })
 		KafkaTransactionManager tm = new KafkaTransactionManager(pf);
 		containerProps.setTransactionManager(tm);
 		KafkaMessageListenerContainer<Integer, String> container =
