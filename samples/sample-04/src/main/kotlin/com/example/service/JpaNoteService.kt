e0bd4d54b9f35b5b1d056e1455355edbe00327d4
Remus Richard Dumitrache <Remus.Richard@autentia.com>
2019-01-05 19:43:14 +0100

    copyright notice and spring plugin dependency version

--- samples/sample-04/src/main/kotlin/com/example/service/JpaNoteService.kt
+++ samples/sample-04/src/main/kotlin/com/example/service/JpaNoteService.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.service
 
 import arrow.core.Option
@@ -21,7 +36,7 @@ class JpaNoteService : NoteService {
     private lateinit var kafkaTemplate: KafkaTemplate<Any, Any>
 
     override fun getAllNotes(): Option<NonEmptyList<Note>> =
-        NonEmptyList.fromList(noteRepository.findAll())
+            NonEmptyList.fromList(noteRepository.findAll())
 
     override fun createNote(note: Option<Note>): Note {
         note.map {
