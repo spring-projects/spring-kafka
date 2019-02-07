aa63da5a81b95c93154eef47f57b8e6377af6a08
Gary Russell <grussell@pivotal.io>
2019-02-07 16:19:25 -0500

    @KafkaListener Consumer Property Overrides
    Add the `properties` attribute to the `@KafkaListener` annotation to
    enhance or override the consumer factory properties.

--- spring-kafka/src/main/java/org/springframework/kafka/config/AbstractKafkaListenerContainerFactory.java
+++ spring-kafka/src/main/java/org/springframework/kafka/config/AbstractKafkaListenerContainerFactory.java
@@ -283,8 +283,6 @@ public abstract class AbstractKafkaListenerContainerFactory<C extends AbstractMe
 
 		endpoint.setupListenerContainer(instance, this.messageConverter);
 		initializeContainer(instance, endpoint);
-		instance.getContainerProperties().setGroupId(endpoint.getGroupId());
-		instance.getContainerProperties().setClientId(endpoint.getClientIdPrefix());
 
 		return instance;
 	}
@@ -357,6 +355,11 @@ public abstract class AbstractKafkaListenerContainerFactory<C extends AbstractMe
 		}
 		if (this.applicationEventPublisher != null) {
 			instance.setApplicationEventPublisher(this.applicationEventPublisher);
+		}
+		instance.getContainerProperties().setGroupId(endpoint.getGroupId());
+		instance.getContainerProperties().setClientId(endpoint.getClientIdPrefix());
+		if (endpoint.getConsumerProperties() != null) {
+			instance.getContainerProperties().setConsumerProperties(endpoint.getConsumerProperties());
 		}
 	}
 
