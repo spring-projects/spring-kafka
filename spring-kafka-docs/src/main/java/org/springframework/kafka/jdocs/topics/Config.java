1cb5b5f2e53a6be305cec07e7dd89ef792a34380
Gary Russell <grussell@vmware.com>
2021-02-03 14:46:09 -0500

    Fix Unused Import.

--- spring-kafka-docs/src/main/java/org/springframework/kafka/jdocs/topics/Config.java
+++ spring-kafka-docs/src/main/java/org/springframework/kafka/jdocs/topics/Config.java
@@ -25,7 +25,6 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.config.TopicConfig;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
