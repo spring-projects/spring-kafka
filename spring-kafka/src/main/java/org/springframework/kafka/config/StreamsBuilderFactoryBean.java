54fc84004f95782f6ac42106fe00c6117444262d
Gary Russell <grussell@vmware.com>
2021-04-20 08:41:02 -0400

    GH-1774: kafka-clients 2.8.0 Compatibility
    Resolves https://github.com/spring-projects/spring-kafka/issues/1774
    
    - changes to `BrokerState`
    - suppress some deprecation warnings

--- spring-kafka/src/main/java/org/springframework/kafka/config/StreamsBuilderFactoryBean.java
+++ spring-kafka/src/main/java/org/springframework/kafka/config/StreamsBuilderFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 the original author or authors.
+ * Copyright 2017-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -188,6 +188,7 @@ public class StreamsBuilderFactoryBean extends AbstractFactoryBean<StreamsBuilde
 		this.stateListener = stateListener; // NOSONAR (sync)
 	}
 
+	@SuppressWarnings("deprecation")
 	public void setUncaughtExceptionHandler(Thread.UncaughtExceptionHandler exceptionHandler) {
 		this.uncaughtExceptionHandler = exceptionHandler; // NOSONAR (sync)
 	}
--- spring-kafka/src/main/java/org/springframework/kafka/support/KafkaStreamBrancher.java
+++ spring-kafka/src/main/java/org/springframework/kafka/support/KafkaStreamBrancher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -86,6 +86,7 @@ public final class KafkaStreamBrancher<K, V> {
 	 * @param stream {@code KStream} to split
 	 * @return the provided stream
 	 */
+	@SuppressWarnings("deprecation")
 	public KStream<K, V> onTopOf(KStream<K, V> stream) {
 		if (this.defaultConsumer != null) {
 			this.predicateList.add((k, v) -> true);
