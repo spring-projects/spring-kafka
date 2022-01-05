9538b006c74f09eaebde787c2a9c11b5bd48a455
scruel <scruelt@hotmail.com>
2022-01-05 08:31:48 +0800

    Replace wildcard imports with single class imports

--- spring-kafka/src/main/java/org/springframework/kafka/annotation/ResolvableType.java
+++ spring-kafka/src/main/java/org/springframework/kafka/annotation/ResolvableType.java
@@ -18,7 +18,11 @@ package org.springframework.kafka.annotation;
 
 import org.springframework.util.MimeTypeUtils;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Used to define the content type of {@code Message} with {@code MediaMessageResolver}.
