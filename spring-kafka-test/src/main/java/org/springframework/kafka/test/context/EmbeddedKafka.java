a7084c4d546d5516241f1a27cb46318dd9ec78b7
Adrian Chlebosz <chlebosz.adrian@gmail.com>
2022-03-29 11:43:07 +0200

    GH-2059: remove unused import

--- spring-kafka-test/src/main/java/org/springframework/kafka/test/context/EmbeddedKafka.java
+++ spring-kafka-test/src/main/java/org/springframework/kafka/test/context/EmbeddedKafka.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 the original author or authors.
+ * Copyright 2017-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,6 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.time.Duration;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 
