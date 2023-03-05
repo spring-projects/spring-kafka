4df01286dd1b810dc0e9a4a2eec8fac0b286813e
AgileTec Solutions <28709874+xodiumluma@users.noreply.github.com>
2023-03-06 06:04:00 +1100

    [learn] add another class

---
+++ __learn__/sample/sample-01/src/main/java/com/common/Foo2.java
@@ -0,0 +1,11 @@
+package com.common;
+public class Foo2 {
+  private String foo;
+  public Foo2() {}
+  public Foo2(String foo) { this.foo = foo; }
+  public String getFoo() { return this.foo; }
+  @Override
+  public String toString() {
+    return "Foo2 [foo=]" + this.foo + "]";
+  }
+}
