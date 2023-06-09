a9d6f76634774aa58a3842e1c722bdc4a7bb1e77
Lee Jaeheon <ti6419@gmail.com>
2023-06-09 11:11:26 +0900

    test: Fix checkstyle violations (#2692)

--- spring-kafka-test/src/test/java/org/springframework/kafka/test/condition/WithNestedClassContextTests.java
+++ spring-kafka-test/src/test/java/org/springframework/kafka/test/condition/WithNestedClassContextTests.java
@@ -1,21 +1,37 @@
+/*
+ * Copyright 2021-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.kafka.test.condition;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.kafka.test.condition.WithNestedClassContextTests.Config;
-
 @EmbeddedKafka
-@SpringJUnitConfig(Config.class)
+@SpringJUnitConfig(WithNestedClassContextTests.Config.class)
 class WithNestedClassContextTests {
 
 	private static final AtomicInteger counter = new AtomicInteger();
