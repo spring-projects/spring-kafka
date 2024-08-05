04500da2889438a6711bb692186b2fe435f19b7f
lokeshAlamuri <lokesh.alamuri@gmail.com>
2024-08-05 19:35:48 +0530

    Fix ContainerGroupSequencer stopParentAndCheckGroup method
    This commit would fix the issue when exactly the ConcurrentContainer has to be stopped. As per the earlier logic, running status would not be set to false if any of the container is stopped. This is not correct and modified the logic to set running status to false even if one of the container is stopped. So, it is sufficient to call directly stop API on parent container that would internally check if all the containers are stopped and would execute the callback accordingly.

--- spring-kafka/src/main/java/org/springframework/kafka/listener/ContainerGroupSequencer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/ContainerGroupSequencer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2021-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -153,24 +153,22 @@ public class ContainerGroupSequencer implements ApplicationContextAware,
 	}
 
 	private synchronized void stopParentAndCheckGroup(MessageListenerContainer parent) {
-		if (parent.isRunning()) {
-			LOGGER.debug(() -> "Stopping: " + parent);
-			parent.stop(() -> {
-				if (this.currentGroup != null) {
-					LOGGER.debug(() -> "Checking group: " + this.currentGroup.toString());
-					if (this.currentGroup.allStopped()) {
-						if (this.iterator.hasNext()) {
-							this.currentGroup = this.iterator.next();
-							LOGGER.debug(() -> "Starting next group: " + this.currentGroup);
-							this.currentGroup.start();
-						}
-						else {
-							this.currentGroup = null;
-						}
+		LOGGER.debug(() -> "Stopping: " + parent);
+		parent.stop(() -> {
+			if (this.currentGroup != null) {
+				LOGGER.debug(() -> "Checking group: " + this.currentGroup.toString());
+				if (this.currentGroup.allStopped()) {
+					if (this.iterator.hasNext()) {
+						this.currentGroup = this.iterator.next();
+						LOGGER.debug(() -> "Starting next group: " + this.currentGroup);
+						this.currentGroup.start();
+					}
+					else {
+						this.currentGroup = null;
 					}
 				}
-			});
-		}
+			}
+		});
 	}
 
 	@Override
