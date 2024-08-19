7e8d325297c78f3fcd2a7b7f584894ac8e1d67f9
Soby Chacko <soby.chacko@broadcom.com>
2024-08-19 10:56:49 -0400

    GH-3425: Return modified map from AnnotationEnhancer (#3428)
    Fixes: #3425
    
    Implement fix for issue #3425 by modifying AnnotationEnhancer to return
    the updated map after processing. This change ensures that all
    annotation enhancements are properly reflected in the returned result.
    
    Modifying an existing test to verify this behavior.
    
    **Auto-cherry-pick to `3.2.x` & `3.1.x`**

--- spring-kafka/src/main/java/org/springframework/kafka/annotation/KafkaListenerAnnotationBeanPostProcessor.java
+++ spring-kafka/src/main/java/org/springframework/kafka/annotation/KafkaListenerAnnotationBeanPostProcessor.java
@@ -139,6 +139,8 @@ import org.springframework.validation.Validator;
  * @author Filip Halemba
  * @author Tomaz Fernandes
  * @author Wang Zhiyang
+ * @author Sanghyeok An
+ * @author Soby Chacko
  *
  * @see KafkaListener
  * @see KafkaListenerErrorHandler
@@ -351,7 +353,7 @@ public class KafkaListenerAnnotationBeanPostProcessor<K, V>
 					for (AnnotationEnhancer enh : enhancers) {
 						newAttrs = enh.apply(newAttrs, element);
 					}
-					return attrs;
+					return newAttrs;
 				};
 			}
 		}
