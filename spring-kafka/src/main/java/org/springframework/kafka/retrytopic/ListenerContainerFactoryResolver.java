bfc5d20b4afd223619ba9798240fad1a4b868656
lokeshAlamuri <lokesh.alamuri@gmail.com>
2024-08-31 16:51:54 +0530

    Refactor code
    Following code is refactored.
    
    1) `generateFromSleepingBackOffPolicy` method in `org.springframework.kafka.retrytopic.BackOffValuesGenerator`
    
    Removed the code to set the `maxBackOffPeriod`, if the BackOffPolicy is `UniformRandomBackOffPolicy`. This is not required since, when sleeper is added to the `UniformRandomBackOffPolicy` `maxBackOffPeriod` is not modified. This could be an issue with the earlier versions of spring-retry. But, latest version of spring-kafka 3.3 and spring-retry 2.0.8, this is not an issue.
    
    2) Modified `addIfAbsent` in `org.springframework.kafka.retrytopic.ListenerContainerFactoryResolver.Cache` to use `putIfAbsent`, rather than explictly checking for the key, if it is not there than adding it to the `HashMap`.

--- spring-kafka/src/main/java/org/springframework/kafka/retrytopic/ListenerContainerFactoryResolver.java
+++ spring-kafka/src/main/java/org/springframework/kafka/retrytopic/ListenerContainerFactoryResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2022 the original author or authors.
+ * Copyright 2018-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -205,9 +205,7 @@ public class ListenerContainerFactoryResolver {
 																ConcurrentKafkaListenerContainerFactory<?, ?> resolvedFactory) {
 			synchronized (this.cacheMap) {
 				Key key = cacheKey(factoryFromKafkaListenerAnnotation, config);
-				if (!this.cacheMap.containsKey(key)) {
-					this.cacheMap.put(key, resolvedFactory);
-				}
+				this.cacheMap.putIfAbsent(key, resolvedFactory);
 				return resolvedFactory;
 			}
 		}
