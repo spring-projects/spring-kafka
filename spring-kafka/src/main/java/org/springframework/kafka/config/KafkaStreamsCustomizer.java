52019c239853f1d701ec7409cce5bd9b73db8074
Almog Gavra <almog.gavra@gmail.com>
2024-09-26 17:02:30 -0700

    GH-3515: Support custom `KafkaStreams` implementations
    Fixes: #3515 
    
    https://github.com/spring-projects/spring-kafka/issues/3515
    
    - Extend `KafkaStreamsCustomizer` with `initKafkaStreams` method
    - Update `StreamsBuilderFactoryBean` to use the new method
    - Update documentation and what's-new section
    - Fix formatting and address checkstyle issues

--- spring-kafka/src/main/java/org/springframework/kafka/config/KafkaStreamsCustomizer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/config/KafkaStreamsCustomizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 the original author or authors.
+ * Copyright 2018-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,17 @@
 
 package org.springframework.kafka.config;
 
+import java.util.Properties;
+
+import org.apache.kafka.streams.KafkaClientSupplier;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.Topology;
 
 /**
  * Callback interface that can be used to configure {@link KafkaStreams} directly.
  *
  * @author Nurettin Yilmaz
+ * @author Almog Gavra
  *
  * @since 2.1.5
  *
@@ -30,6 +35,32 @@ import org.apache.kafka.streams.KafkaStreams;
 @FunctionalInterface
 public interface KafkaStreamsCustomizer {
 
+	/**
+	 * Customize the instantiation of the {@code KafkaStreams} instance. This
+	 * happens before the modifications made by {@link StreamsBuilderFactoryBean}.
+	 *
+	 * @param topology the full topology
+	 * @param properties the configuration properties
+	 * @param clientSupplier the client supplier
+	 *
+	 * @return a new instance of {@link KafkaStreams}
+	 *
+	 * @since 3.3.0
+	 */
+	default KafkaStreams initKafkaStreams(
+			Topology topology,
+			Properties properties,
+			KafkaClientSupplier clientSupplier
+	) {
+		return new KafkaStreams(topology, properties, clientSupplier);
+	}
+
+	/**
+	 * Customize the instance of {@code KafkaStreams} after {@link StreamsBuilderFactoryBean}
+	 * has applied its default configurations.
+	 *
+	 * @param kafkaStreams the instantiated Kafka Streams instance
+	 */
 	void customize(KafkaStreams kafkaStreams);
 
 }
