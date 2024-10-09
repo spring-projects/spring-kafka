3f45fc01f2db6a083b94930e71a6ac359bdc9dfa
Soby Chacko <soby.chacko@broadcom.com>
2024-10-09 10:14:24 -0400

    GH-3544: Support SpEL in @KafkaListener containerPostProcessor (#3548)
    Fixes: #3544
    
    https://github.com/spring-projects/spring-kafka/issues/3544
    
    - Enhance resolveContainerPostProcessor method in KafkaListenerAnnotationBeanPostProcessor
      to evaluate SpEL expressions
    - Verify containerPostProcessor property in KafkaListener annotation can be specified as a SpEL expression
    
    **Auto-cherry-pick to `3.2.x` & `3.1.x`**

--- spring-kafka/src/test/java/org/springframework/kafka/listener/ContainerCustomizationTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/listener/ContainerCustomizationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the original author or authors.
+ * Copyright 2019-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,7 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
  * Tests for container customizations.
  *
  * @author Francois Rosiere
+ * @author Soby Chacko
  * @since 3.1
  */
 @SuppressWarnings("unused")
@@ -129,7 +130,7 @@ class ContainerCustomizationTests {
 				id = CONTAINER_CUSTOMIZER_AND_POST_PROCESSOR,
 				topics = TOPIC,
 				containerFactory = "containerFactoryWithCustomizer",
-				containerPostProcessor = "infoContainerPostProcessor")
+				containerPostProcessor = "#{__listener.infoContainerPostProcessor}")
 		public void containerCustomizerAndPostProcessor(String foo) {
 		}
 
