42467c866bb6472935c3ad4f9d29a2a7b0216383
François Rosière <francois.rosiere@gmail.com>
2025-03-26 16:55:23 +0100

    GH-3808: ReplyingKafkaTemplate observation on reply
    Fixes: #3808
    Issue link: https://github.com/spring-projects/spring-kafka/issues/3808
    
    Since  `ReplyingKafkaTemplate` is a `BatchMessageListener` for the provided listener container,
    we cannot rely on the observation from that container.
    
    * Implement consumer observation from the `ReplyingKafkaTemplate.BatchMessageListener`.
    
    Signed-off-by: Francois Rosiere <francois.rosiere@gmail.com>
    
    [artem.bilan@broadcom.com Improve commit message]
    
    Signed-off-by: Artem Bilan <artem.bilan@broadcom.com>
    
    # Conflicts:
    #	spring-kafka/src/main/java/org/springframework/kafka/core/KafkaTemplate.java
    #	spring-kafka/src/test/java/org/springframework/kafka/support/micrometer/ObservationTests.java

--- spring-kafka/src/main/java/org/springframework/kafka/core/KafkaTemplate.java
+++ spring-kafka/src/main/java/org/springframework/kafka/core/KafkaTemplate.java
@@ -103,6 +103,7 @@ import org.springframework.util.StringUtils;
  * @author Soby Chacko
  * @author Gurps Bassi
  * @author Valentina Armenise
+ * @author Francois Rosiere
  */
 public class KafkaTemplate<K, V> implements KafkaOperations<K, V>, ApplicationContextAware, BeanNameAware,
 		ApplicationListener<ContextStoppedEvent>, DisposableBean, SmartInitializingSingleton {
@@ -457,6 +458,15 @@ public class KafkaTemplate<K, V> implements KafkaOperations<K, V>, ApplicationCo
 	}
 
 	/**
+	 * Return the {@link ObservationRegistry} used by the template.
+	 * @return the observation registry
+	 * @since 3.2.9
+	 */
+	protected ObservationRegistry getObservationRegistry() {
+		return this.observationRegistry;
+	}
+
+	/**
 	 * Return the {@link KafkaAdmin}, used to find the cluster id for observation, if
 	 * present.
 	 * @return the kafkaAdmin
@@ -520,8 +530,13 @@ public class KafkaTemplate<K, V> implements KafkaOperations<K, V>, ApplicationCo
 		return removeLeadingAndTrailingBrackets(adminServers);
 	}
 
+	/**
+	 * Return the cluster id, if available.
+	 * @return the cluster id.
+	 * @since 3.2.9
+	 */
 	@Nullable
-	private String clusterId() {
+	protected String clusterId() {
 		if (this.kafkaAdmin != null && this.clusterId == null) {
 			this.clusterIdLock.lock();
 			try {
