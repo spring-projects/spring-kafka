11797d957fd827c7090c1bc2220264ae226ae27b
ChickenchickenLove <ojt90902@naver.com>
2025-04-30 00:47:54 +0900

    GH-3869: Fail if no `@KafkaHandler` on class-level `@KafkaListener`
    Fixes: #3869
    Issue link: https://github.com/spring-projects/spring-kafka/issues/3869
    
    * Update spring-kafka/src/main/java/org/springframework/kafka/annotation/KafkaListenerAnnotationBeanPostProcessor.java
    
    Signed-off-by: ChickenchickenLove <ojt90902@naver.com>
    Co-authored-by: Artem Bilan <artem.bilan@broadcom.com>
    (cherry picked from commit 6682837501d81866d8aad9804160c9035bf99f84)

---
+++ spring-kafka/src/test/java/org/springframework/kafka/annotation/KafkaListenerAnnotationBeanPostProcessorTests.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.kafka.annotation;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+/**
+ * @author Sanghyeok An
+ *
+ * @since 4.0.0
+ */
+
+class KafkaListenerAnnotationBeanPostProcessorTests {
+
+	@Test
+	void ctx_should_be_fail_to_register_bean_when_no_listener_methods_exist() {
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(TestConfig.class);
+
+		assertThatExceptionOfType(BeanCreationException.class)
+				.isThrownBy(ctx::refresh)
+				.withRootCauseInstanceOf(IllegalStateException.class)
+				.withMessageContaining("No Kafka listener methods in bean:")
+				.withMessageContaining("NoHandlerMethodListener");
+
+	}
+
+	@EnableKafka
+	@Configuration
+	static class TestConfig {
+
+		@Component
+		@KafkaListener
+		static class NoHandlerMethodListener {
+
+			public void listen(String message) {
+			}
+		}
+
+	}
+
+}
