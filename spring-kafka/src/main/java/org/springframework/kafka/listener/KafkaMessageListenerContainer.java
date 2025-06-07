5f623d63e09c15f1bed569de4e513f0f8c17462f
Igor Macedo Quintanilha <igor.quintanilha@teya.com>
2025-06-07 10:46:49 +0100

    change the if clause

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
@@ -2446,7 +2446,7 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 
 
 			try {
-				if (this.containerProperties.isRecordObservationsInBatch() ) {
+				if (this.containerProperties.isObservationEnabled() ) {
 					invokeBatchWithIndividualRecordObservation(recordList);
 				}
 
