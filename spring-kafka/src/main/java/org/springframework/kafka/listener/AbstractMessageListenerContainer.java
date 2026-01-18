9e0ef68687d4d80a190ecb2c1ae0dd8498e204c6
david-parkk <ji0513ji@naver.com>
2026-01-18 16:40:35 +0900

    Remove redundant @NonNull from getBeanName()

--- spring-kafka/src/main/java/org/springframework/kafka/listener/AbstractMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/AbstractMessageListenerContainer.java
@@ -214,7 +214,6 @@ public abstract class AbstractMessageListenerContainer<K, V>
 	 * Return the bean name.
 	 * @return the bean name.
 	 */
-	@NonNull
 	public String getBeanName() {
 		return this.beanName;
 	}
