c0d3c61980c0ff7cf4d0915c81f60d5c41715d3c
Artem Bilan <artem.bilan@broadcom.com>
2026-04-18 13:06:51 -0400

    GH-4377: Fix `asyncReplies` calculation for container (#4401)
    * GH-4377: Fix `asyncReplies` calculation for container
    
    Fixes: https://github.com/spring-projects/spring-kafka/issues/4377
    
    The `KafkaMessageListenerContainer.ListenerConsumer` determines its `asyncReplies`
    against `listener instanceof AsyncRepliesAware`.
    However, in case of retryable topics configuration, or just `factory.setRecordFilterStrategy()`,
    the provided `listener` is going to be wrapped with a `DelegatingMessageListener`
    or even stack of them, e.g., in case of retryable topics and record filtering combination
    
    * Implement `AsyncRepliesAware` contract on the `DelegatingMessageListener`
    with simple delegation to the `getDelegate()`
    * There is not need in new tests, and modification of existing configuration,
     based on `CompletableFuture` and `Mono`, is enough to prove that new feature works as expected.
    Previously those tests enforced `AckMode.MANUAL` and `asyncAcks` through configuration to work out `asyncReplies` behavior.
    With the fix we got them through fallback according to the `isAsyncReplies()` state of the
    ultimate listener
    
    The `AsyncRepliesAware` contract goes down in the history to the `3.3.x` version.
    Therefore, applying this fix into that branch feels legit.
    Moreover, it is not a breaking change due to the `default` methods feature in Java interfaces.
    
    **Auto-cherry-pick to `4.0.x` & `3.3.x`**
    
    * Remove unused import from the AsyncMonoRetryTopicScenarioTests

--- spring-kafka/src/test/java/org/springframework/kafka/retrytopic/AsyncMonoRetryTopicScenarioTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/retrytopic/AsyncMonoRetryTopicScenarioTests.java
@@ -41,7 +41,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.listener.ContainerProperties.AckMode;
 import org.springframework.kafka.listener.KafkaListenerErrorHandler;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
@@ -455,7 +454,7 @@ public class AsyncMonoRetryTopicScenarioTests {
 	}
 
 	@EnableKafka
-	@Configuration
+	@Configuration(proxyBeanMethods = false)
 	static class KafkaConsumerConfig {
 
 		@Autowired
@@ -474,7 +473,7 @@ public class AsyncMonoRetryTopicScenarioTests {
 		ConcurrentKafkaListenerContainerFactory<String, String> retryTopicListenerContainerFactory(
 				ConsumerFactory<String, String> consumerFactory) {
 
-			ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+			var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
 			ContainerProperties props = factory.getContainerProperties();
 			props.setIdleEventInterval(100L);
 			props.setPollTimeout(50L);
@@ -490,15 +489,9 @@ public class AsyncMonoRetryTopicScenarioTests {
 		ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
 				ConsumerFactory<String, String> consumerFactory) {
 
-			ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+			var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
 			factory.setConsumerFactory(consumerFactory);
 			factory.setConcurrency(1);
-			factory.setContainerCustomizer(container -> {
-				if (container.getListenerId().startsWith("manual")) {
-					container.getContainerProperties().setAckMode(AckMode.MANUAL);
-					container.getContainerProperties().setAsyncAcks(true);
-				}
-			});
 			return factory;
 		}
 
