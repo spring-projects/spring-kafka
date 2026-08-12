27dec1fd0b0912ec2a8c13c62dc4851a3f11ac0f
Soby Chacko <soby.chacko@broadcom.com>
2026-08-12 14:40:58 -0400

    Fix compile error in ShareKafkaMessageListenerContainerUnitTests
    LenientStubber does not have willAnswer(); use doAnswer().when() instead.
    
    Signed-off-by: Soby Chacko <soby.chacko@broadcom.com>

--- spring-kafka/src/test/java/org/springframework/kafka/listener/ShareKafkaMessageListenerContainerUnitTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/listener/ShareKafkaMessageListenerContainerUnitTests.java
@@ -712,10 +712,10 @@ public class ShareKafkaMessageListenerContainerUnitTests {
 				.willReturn(firstConsumer);
 		given(this.shareConsumerFactory.createShareConsumer(any(), eq("failedStartContainer-1"), any()))
 				.willReturn(secondConsumer);
-		lenient().willAnswer(invocation -> {
+		lenient().doAnswer(invocation -> {
 			Thread.sleep(200);
 			return new ConsumerRecords<>(Collections.emptyMap(), Map.of());
-		}).given(firstConsumer).poll(Duration.ofMillis(1000));
+		}).when(firstConsumer).poll(Duration.ofMillis(1000));
 
 		ShareKafkaMessageListenerContainer<String, String> container =
 				new ShareKafkaMessageListenerContainer<>(this.shareConsumerFactory, containerProperties);
